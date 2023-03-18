d68226e841cde430fa08e10cfa1d67195501e86c
James Thorpe <james@pawsforthorpe.co.uk>
2023-03-18 20:36:25 +0000

    node list flags

--- src/client/components/NodeList.vue
+++ src/client/components/NodeList.vue
@@ -10,6 +10,8 @@
                     <th>Node Number</th>
                     <th>Manufacturer ID</th>
                     <th>Module ID</th>
+                    <th></th>
+
                 </tr>
             </thead>
             <tbody>
@@ -21,6 +23,28 @@
                 <td>{{ node.nodeNumber }}</td>
                 <td>{{ node.manufacturerId }}</td>
                 <td>{{ node.moduleId }}</td>
+                <td>
+                    <v-tooltip text="Indicates whether this is a consumer node" location="bottom">
+                        <template v-slot:activator="{props}">
+                            <v-icon v-bind="props" icon="mdi-arrow-bottom-right-thick" :class="{'iconOn': node.isConsumer}"></v-icon>
+                        </template>
+                    </v-tooltip>
+                    <v-tooltip text="Indicates whether this is a producer node" location="bottom">
+                        <template v-slot:activator="{props}">
+                            <v-icon v-bind="props" icon="mdi-arrow-top-right-thick" :class="{'iconOn': node.isProducer}"></v-icon>
+                        </template>
+                    </v-tooltip>
+                    <v-tooltip text="Indicates whether this node is in FLiM mode" location="bottom">
+                        <template v-slot:activator="{props}">
+                            <v-icon v-bind="props" icon="mdi-cog-outline" :class="{'iconOn': node.isFlim}"></v-icon>        
+                        </template>
+                    </v-tooltip>
+                    <v-tooltip text="Indicates whether this node can be updated remotely using a bootloader">
+                        <template v-slot:activator="{props}">
+                            <v-icon v-bind="props" icon="mdi-download-box" :class="{'iconOn': node.isBootloadable}"></v-icon>
+                        </template>
+                    </v-tooltip>
+                </td>
             </tr>
             </tbody>
         </v-table>
@@ -59,3 +83,8 @@ export default {
     }
 }
 </script>
+<style type="scss">
+.iconOn {
+    color: green;
+}
+</style>
--- src/client/config/cbusnetwork.ts
+++ src/client/config/cbusnetwork.ts
@@ -3,10 +3,15 @@ import { Socket } from "../api/socket";
 import { reactive } from "vue";
 
 export type CbusNode = {
+    name: string,
     nodeNumber: number,
     manufacturerId: number,
     moduleId: number,
-    name: string
+    isConsumer: boolean,
+    isProducer: boolean,
+    isFlim: boolean,
+    isBootloadable: boolean
+
 };
 
 export const Network = reactive({
@@ -20,10 +25,14 @@ export const Network = reactive({
         const existingNode = this.nodes.find(n => n.nodeNumber == pnn.nodeNumber);
         if (existingNode === undefined) {
             this.nodes.push({
+                name: `Node ${pnn.nodeNumber}`,
                 nodeNumber: pnn.nodeNumber,
                 manufacturerId: pnn.manufId,
                 moduleId: pnn.moduleId,
-                name: `Node ${pnn.nodeNumber}`
+                isConsumer: pnn.nodeFlags & 1,
+                isProducer: pnn.nodeFlags & 2,
+                isFlim: pnn.nodeFlags & 4,
+                isBootloadable: pnn.nodeFlags & 8
             });
         } else {
             existingNode.manufacturerId = pnn.manufId;
